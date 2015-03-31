d270b0882863592f84abd7c8b3b071ac1f22bcbc
Cory Redmond <acecheese@live.co.uk>
2015-03-31 05:04:22 +0100

    Initial commit.

---
+++ src/main/java/eu/ac3_servers/dev/joinchanger/JoinChangerPlugin.java
@@ -0,0 +1,58 @@
+package eu.ac3_servers.dev.joinchanger;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.collect.Maps;
+
+public class JoinChangerPlugin extends JavaPlugin implements Listener {
+	
+	private HashMap<String, String> joinMessages = Maps.newHashMap();
+	
+	private boolean sendIfNone = false;
+	
+	@Override
+	public void onEnable() {
+		
+		File config = new File(getDataFolder(), "config.yml");
+		if(!config.exists()) saveDefaultConfig();
+		
+		sendIfNone = getConfig().getBoolean("send_if_none", false);
+		
+		ConfigurationSection section = getConfig().getConfigurationSection("groups");
+		for(String key : section.getKeys(false))
+			joinMessages.put(key.toUpperCase(), colour(section.getString(key)));
+		
+		getServer().getPluginManager().registerEvents(this, this);
+		
+	}
+	
+	private static String colour(String string) {
+		return ChatColor.translateAlternateColorCodes('&', string);
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		
+		for(String group : joinMessages.keySet())
+			if(e.getPlayer().hasPermission("joinchanger.group." + group)) {
+				String joinMessage = joinMessages.get(group);
+				joinMessage = joinMessage.replace("%name%", e.getPlayer().getName());
+				joinMessage = joinMessage.replace("%displayname%", e.getPlayer().getDisplayName());
+				e.setJoinMessage(joinMessage);
+				return;
+			}
+		
+		if(!sendIfNone)
+			e.setJoinMessage(null);
+		
+	}
+
+}
